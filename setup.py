5791882e3bcc5372510493d9f068071f4b2a740e
tbohne <tim.bohne@dfki.de>
2022-11-28 12:42:45 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+__version__ = '0.0.1'
+URL = 'https://github.com/tbohne/dtc_parser'
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='dtc_parser',
+    version=__version__,
+    description='Parser for diagnostic trouble codes (DTCs) used by vehicle onboard diagnosis (OBD).',
+    author='Tim Bohne',
+    author_email='tim.bohne@dfki.de',
+    url=URL,
+    download_url=f'{URL}/archive/{__version__}.tar.gz',
+    keywords=[
+        'parser',
+        'DTC',
+        'OBD'
+    ],
+    python_requires='>=3.7',
+    install_requires=required,
+    packages=find_packages(),
+    include_package_data=True,
+)
